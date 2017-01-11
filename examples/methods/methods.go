cd4b69b323233264a2f78554fa990b18baf1f98f
oohira <oohira.dev@gmail.com>
2017-01-12 00:45:20 +0900

    Translate examples/methods

--- examples/methods/methods.go
+++ examples/methods/methods.go
@@ -1,4 +1,4 @@
-// Go supports _methods_ defined on struct types.
+// Go は構造体型に定義する _メソッド (methods)_ をサポートします。
 
 package main
 
@@ -8,13 +8,14 @@ type rect struct {
     width, height int
 }
 
-// This `area` method has a _receiver type_ of `*rect`.
+// この `area` メソッドは、 `*rect` を _レシーバ型 (receiver type)_
+// としてもちます。
 func (r *rect) area() int {
     return r.width * r.height
 }
 
-// Methods can be defined for either pointer or value
-// receiver types. Here's an example of a value receiver.
+// メソッドは、ポインタまたは値のどちらのレシーバ型に対しても
+// 定義することができます。これは値レシーバの例です。
 func (r rect) perim() int {
     return 2*r.width + 2*r.height
 }
@@ -22,15 +23,15 @@ func (r rect) perim() int {
 func main() {
     r := rect{width: 10, height: 5}
 
-    // Here we call the 2 methods defined for our struct.
+    // 構造体に定義された 2 つのメソッドを呼び出します。
     fmt.Println("area: ", r.area())
     fmt.Println("perim:", r.perim())
 
-    // Go automatically handles conversion between values
-    // and pointers for method calls. You may want to use
-    // a pointer receiver type to avoid copying on method
-    // calls or to allow the method to mutate the
-    // receiving struct.
+    // Go は、メソッド呼び出しに対する値・ポインタ間の変換を
+    // 自動的に扱います。
+    // メソッドを呼び出す時の構造体の不要なコピーを避けたり、
+    // 元の構造体自体を変更したりするためには、
+    // ポインタレシーバ型を使ってください。
     rp := &r
     fmt.Println("area: ", rp.area())
     fmt.Println("perim:", rp.perim())
--- examples/methods/methods.sh
+++ examples/methods/methods.sh
@@ -4,5 +4,5 @@ perim: 30
 area:  50
 perim: 30
 
-# Next we'll look at Go's mechanism for grouping and
-# naming related sets of methods: interfaces.
+# 次に、関連するメソッド群をグループ化して名前を付ける
+# Go の仕組みとして、インターフェースを見ていきます。
