0c8ed3036a0d24ff5a73cdb14d2eb560cc83ac6e
oohira <oohira.dev@gmail.com>
2017-02-12 21:15:05 +0900

    Translate examples/time

--- examples/time/time.go
+++ examples/time/time.go
@@ -1,5 +1,5 @@
-// Go offers extensive support for times and durations;
-// here are some examples.
+// Go は時刻と期間に対する広範なサポートを提供します。
+// 以下は、いくつかの例です。
 
 package main
 
@@ -9,19 +9,17 @@ import "time"
 func main() {
     p := fmt.Println
 
-    // We'll start by getting the current time.
+    // 現在時刻を取得するところから始めます。
     now := time.Now()
     p(now)
 
-    // You can build a `time` struct by providing the
-    // year, month, day, etc. Times are always associated
-    // with a `Location`, i.e. time zone.
+    // 年、月、日などを与えて、`time` 構造体を作ることができます。
+    // 時刻は常に `Location` すなわちタイムゾーンとひも付きます。
     then := time.Date(
         2009, 11, 17, 20, 34, 58, 651387237, time.UTC)
     p(then)
 
-    // You can extract the various components of the time
-    // value as expected.
+    // 時刻の様々な要素を期待通り取得することができます。
     p(then.Year())
     p(then.Month())
     p(then.Day())
@@ -31,31 +29,28 @@ func main() {
     p(then.Nanosecond())
     p(then.Location())
 
-    // The Monday-Sunday `Weekday` is also available.
+    // 月曜から日曜までの曜日を取得する `Weekday` も使えます。
     p(then.Weekday())
 
-    // These methods compare two times, testing if the
-    // first occurs before, after, or at the same time
-    // as the second, respectively.
+    // これらのメソッドは 2 つの時刻を比較し、1 つ目の時刻が
+    // 2 つ目の時刻の前か、後か、あるいは同じかを判定します。
     p(then.Before(now))
     p(then.After(now))
     p(then.Equal(now))
 
-    // The `Sub` methods returns a `Duration` representing
-    // the interval between two times.
+    // `Sub` メソッドは、2 つの時刻の間隔を表す
+    // `Duration` を返します。
     diff := now.Sub(then)
     p(diff)
 
-    // We can compute the length of the duration in
-    // various units.
+    // 期間の長さは、様々な単位に換算できます。
     p(diff.Hours())
     p(diff.Minutes())
     p(diff.Seconds())
     p(diff.Nanoseconds())
 
-    // You can use `Add` to advance a time by a given
-    // duration, or with a `-` to move backwards by a
-    // duration.
+    // `Add` を使って、指定した期間だけ時刻を進めたり、
+    // あるいは `-` を使って戻したりすることができます。
     p(then.Add(diff))
     p(then.Add(-diff))
 }
