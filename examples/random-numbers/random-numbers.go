b1cf208a3acc78773442c280c48bb9ce37069944
oohira <oohira.dev@gmail.com>
2017-02-19 22:08:41 +0900

    Translate examples/random-numbers

--- examples/random-numbers/random-numbers.go
+++ examples/random-numbers/random-numbers.go
@@ -1,6 +1,6 @@
-// Go's `math/rand` package provides
-// [pseudorandom number](http://en.wikipedia.org/wiki/Pseudorandom_number_generator)
-// generation.
+// Go の `math/rand` パッケージは、
+// [擬似乱数](http://en.wikipedia.org/wiki/Pseudorandom_number_generator)
+// の生成をサポートします。
 
 package main
 
@@ -10,38 +10,38 @@ import "math/rand"
 
 func main() {
 
-    // For example, `rand.Intn` returns a random `int` n,
-    // `0 <= n < 100`.
+    // 例えば、`rand.Intn` は、`0 <= n < 100`
+    // となる `int` 型の乱数 `n` を返します。
     fmt.Print(rand.Intn(100), ",")
     fmt.Print(rand.Intn(100))
     fmt.Println()
 
-    // `rand.Float64` returns a `float64` `f`,
-    // `0.0 <= f < 1.0`.
+    // `rand.Float64` は、`0.0 <= f < 1.0`
+    // となる `float64` 型の乱数 `f` を返します。
     fmt.Println(rand.Float64())
 
-    // This can be used to generate random floats in
-    // other ranges, for example `5.0 <= f' < 10.0`.
+    // 次のようにすれば、例えば `5.0 <= f < 10.0`
+    // のように異なる範囲の乱数を生成することもできます。
     fmt.Print((rand.Float64()*5)+5, ",")
     fmt.Print((rand.Float64() * 5) + 5)
     fmt.Println()
 
-    // The default number generator is deterministic, so it'll
-    // produce the same sequence of numbers each time by default.
-    // To produce varying sequences, give it a seed that changes.
-    // Note that this is not safe to use for random numbers you
-    // intend to be secret, use `crypto/rand` for those.
+    // デフォルトの乱数生成器は決定性があります。すなわち、
+    // 生成される一連の数列は毎回同じになります。
+    // 異なる数列を生成するには、異なるシードを与えます。
+    // なお、これを秘密にしたい乱数のために使うのは安全でない、
+    // ということに注意してください。
+    // そういった場合は、`crypto/rand` を使います。
     s1 := rand.NewSource(time.Now().UnixNano())
     r1 := rand.New(s1)
 
-    // Call the resulting `rand.Rand` just like the
-    // functions on the `rand` package.
+    // 返された `rand.Rand` に対して、`rand`
+    // パッケージの関数と同じように関数を呼び出します。
     fmt.Print(r1.Intn(100), ",")
     fmt.Print(r1.Intn(100))
     fmt.Println()
 
-    // If you seed a source with the same number, it
-    // produces the same sequence of random numbers.
+    // 同じ値をシードとして与えると、同じ乱数列を生成します。
     s2 := rand.NewSource(42)
     r2 := rand.New(s2)
     fmt.Print(r2.Intn(100), ",")
--- examples/random-numbers/random-numbers.sh
+++ examples/random-numbers/random-numbers.sh
@@ -7,6 +7,6 @@ $ go run random-numbers.go
 5,87
 
 
-# See the [`math/rand`](http://golang.org/pkg/math/rand/)
-# package docs for references on other random quantities
-# that Go can provide.
+# Go が提供するその他の乱数については、
+# [`math/rand`](http://golang.org/pkg/math/rand/)
+# パッケージのドキュメントを参照してください。
