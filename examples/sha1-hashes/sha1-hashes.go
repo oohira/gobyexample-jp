64b9678f9cd3ec90b57995efe92b61c92b7a84af
oohira <oohira.dev@gmail.com>
2017-02-22 23:18:53 +0900

    Translate examples/sha1-hashes

--- examples/sha1-hashes/sha1-hashes.go
+++ examples/sha1-hashes/sha1-hashes.go
@@ -1,37 +1,38 @@
-// [_SHA1 hashes_](http://en.wikipedia.org/wiki/SHA-1) are
-// frequently used to compute short identities for binary
-// or text blobs. For example, the [git revision control
-// system](http://git-scm.com/) uses SHA1s extensively to
-// identify versioned files and directories. Here's how to
-// compute SHA1 hashes in Go.
+// [_SHA1 ハッシュ_](http://en.wikipedia.org/wiki/SHA-1)
+// は、バイナリやテキストのかたまりに対して短い ID
+// を計算するのによく使われます。例えば、
+// [Git バージョン管理システム](http://git-scm.com/) は
+// バージョン管理されるファイルやディレクトリを識別するために、
+// SHA1 を広範囲にわたって使用しています。
+// 以下は、Go で SHA1 ハッシュを計算する方法です。
 
 package main
 
-// Go implements several hash functions in various
-// `crypto/*` packages.
+// Go は様々な `crypto/*` パッケージで、
+// 複数のハッシュ関数を実装しています。
 import "crypto/sha1"
 import "fmt"
 
 func main() {
     s := "sha1 this string"
 
-    // The pattern for generating a hash is `sha1.New()`,
-    // `sha1.Write(bytes)`, then `sha1.Sum([]byte{})`.
-    // Here we start with a new hash.
+    // ハッシュを生成するパターンは、`sha1.New()` の次に
+    // `sha1.Write(bytes)`、そして `sha1.Sum([]byte{})`
+    // です。以下では、ハッシュの生成から始めます。
     h := sha1.New()
 
-    // `Write` expects bytes. If you have a string `s`,
-    // use `[]byte(s)` to coerce it to bytes.
+    // `Write` はバイト列を受け取ります。文字列 `s` がある場合は、
+    // `[]byte(s)` を使って強制的にバイト列にします。
     h.Write([]byte(s))
 
-    // This gets the finalized hash result as a byte
-    // slice. The argument to `Sum` can be used to append
-    // to an existing byte slice: it usually isn't needed.
+    // バイト型のスライスとして最終的なハッシュ値を取得します。
+    // `Sum` への引数は、既存のバイト型スライスへハッシュ値を
+    // 追記したい場合に使えますが、通常は必要ありません。
     bs := h.Sum(nil)
 
-    // SHA1 values are often printed in hex, for example
-    // in git commits. Use the `%x` format verb to convert
-    // a hash results to a hex string.
+    // 例えば Git のコミットなど、SHA1 値はよく 16
+    // 進数で表示されます。ハッシュ値を 16 進文字列に変換するには、
+    // `%x` フォーマット指定子を使ってください。
     fmt.Println(s)
     fmt.Printf("%x\n", bs)
 }
--- examples/sha1-hashes/sha1-hashes.sh
+++ examples/sha1-hashes/sha1-hashes.sh
@@ -1,14 +1,14 @@
-# Running the program computes the hash and prints it in
-# a human-readable hex format.
+# プログラムを実行すると、ハッシュが計算され、
+# 人間が読める 16 進フォーマットで表示されます。
 $ go run sha1-hashes.go
 sha1 this string
 cf23df2207d99a74fbe169e3eba035e633b65d94
 
 
-# You can compute other hashes using a similar pattern to
-# the one shown above. For example, to compute MD5 hashes
-# import `crypto/md5` and use `md5.New()`.
+# 先に説明したのと同様のパターンを使って、他のハッシュを計算することができます。
+# 例えば、MD5 ハッシュを計算するには、`crypto/md5` をインポートして
+# `md5.New()` を使います。
 
-# Note that if you need cryptographically secure hashes,
-# you should carefully research
-# [hash strength](http://en.wikipedia.org/wiki/Cryptographic_hash_function)!
+# 暗号論的に安全なハッシュが必要な場合には、
+# [ハッシュの強度](http://en.wikipedia.org/wiki/Cryptographic_hash_function)
+# を注意深く調査すべきである点に注意してください！
