afa31def2ec9f5d09ec6a098073e2ecbd439e2ef
oohira <oohira.dev@gmail.com>
2017-03-06 23:03:05 +0900

    Translate examples/command-line-arguments

--- examples/command-line-arguments/command-line-arguments.go
+++ examples/command-line-arguments/command-line-arguments.go
@@ -1,7 +1,7 @@
-// [_Command-line arguments_](http://en.wikipedia.org/wiki/Command-line_interface#Arguments)
-// are a common way to parameterize execution of programs.
-// For example, `go run hello.go` uses `run` and
-// `hello.go` arguments to the `go` program.
+// [_コマンドライン引数_](http://en.wikipedia.org/wiki/Command-line_interface#Arguments)
+// は、プログラムの実行をパラメーター化する一般的な方法です。
+// 例えば、`go run hello.go` は、`run` と
+// `hello.go` を `go` プログラムの引数として使います。
 
 package main
 
@@ -10,14 +10,14 @@ import "fmt"
 
 func main() {
 
-    // `os.Args` provides access to raw command-line
-    // arguments. Note that the first value in this slice
-    // is the path to the program, and `os.Args[1:]`
-    // holds the arguments to the program.
+    // `os.Args` は、生のコマンドライン引数へのアクセスを提供します。
+    // このスライスの 1 つ目の値は、プログラム自身のパスで、
+    // `os.Args[1:]` がプログラムへの引数を保持することに
+    // 注意してください。
     argsWithProg := os.Args
     argsWithoutProg := os.Args[1:]
 
-    // You can get individual args with normal indexing.
+    // 個々の引数はインデックスを使って通常通り取得できます。
     arg := os.Args[3]
 
     fmt.Println(argsWithProg)
